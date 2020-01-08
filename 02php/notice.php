11d8bb85ae4d5c2e01d6a41edd069c82a1509c82
gkgkgksmfsmfsmf <58929670+gkgkgksmfsmfsmf@users.noreply.github.com>
2020-01-08 11:19:22 +0900

    todayup

---
+++ 02php/notice.php
@@ -0,0 +1,9 @@
+<?include "header.php"; ?>
+<div class="sub notice">
+	<div>notice</div>
+</div>
+<section id="container">
+	<div class="content"></div>
+	<aside class="menu"></aside>
+</section>
+<?include "footer.php"; ?>
